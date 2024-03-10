23c99755d99f6d81a5154c8d5e3e48c8f0cf775e
starrybomp <starry@bomp.dev>
2024-03-10 17:52:04 -0400

    DunGen

---
+++ Scripts/Rooms.gd
@@ -0,0 +1,15 @@
+@tool
+extends Node
+
+var start_rooms = [
+	["res://Scenes/Rooms/sr_campfire.tscn", 26, 26]
+]
+
+var regular_rooms = [
+	["res://Scenes/Rooms/rr_longroom.tscn", 13, 50],
+	["res://Scenes/Rooms/rr_squareyroom.tscn", 10, 10]
+]
+
+var end_rooms = [
+	
+]
--- Scripts/april.gd
+++ Scripts/april.gd
@@ -7,15 +7,16 @@ var max_health = 10
 
 const SPEED = 4.0
 
-@export var player_path: NodePath
+#@export var player_path: NodePath
 @onready var nav_agent = $NavigationAgent3D
 
 func _ready():
-	player = get_node(player_path)
+	player = get_tree().get_nodes_in_group("player")[0]
 	
 func hit(bullet):
 	health -= player.damage
 	$GPUParticles3D.emitting = true
+	attacked = true
 	if health <= 0:
 		$GPUParticles3D.emitting = false
 		$GPUParticles3D.emitting = true
@@ -23,6 +24,10 @@ func hit(bullet):
 		queue_free()
 
 func _process(delta):
+	if attacked:
+		$Label3D.visible = true
+	else:
+		$Label3D.visible = false
 	$Label3D.text = str(health) + "/" + str(max_health)
 	velocity = Vector3.ZERO
 	nav_agent.set_target_position(player.global_transform.origin)
